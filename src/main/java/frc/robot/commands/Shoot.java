e75006db363a1b5983108c53dbddfaaf51b33ba5
Tim Shmidt <timsh007@gmail.com>
2026-01-12 21:05:22 -0800

    removed unnecessary stuff

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -5,8 +5,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.MathUtils.Polynomials;
-import frc.robot.MathUtils.Vector3;
 import frc.robot.subsystems.ShooterSubsystem;
 
 // it will need to rotate swerve and move arm
