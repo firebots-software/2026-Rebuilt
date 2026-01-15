71cd6f7f9a4b5e9533ab914716571ddb289468cc
Saketh <sgiri331@student.fuhsd.org>
2026-01-14 21:14:25 -0800

    gradle spotless changes

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -19,7 +19,6 @@ import java.util.stream.Collectors;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
@@ -77,8 +76,7 @@ public class VisionSubsystem extends SubsystemBase {
 
     // initialize poseEstimator
     // PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
-    poseEstimator =
-        new PhotonPoseEstimator(fieldLayout,cameraToRobot);
+    poseEstimator = new PhotonPoseEstimator(fieldLayout, cameraToRobot);
 
     cameraTitle = cameraID.getLoggingName();
     latestVisionResult = null;
