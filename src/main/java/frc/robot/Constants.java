656d228b6f45d51c12f2da77876cd8050b207d41
arj2cool <arjunv.lzp@gmail.com>
2026-01-19 20:21:08 -0800

    spotless & updates

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -35,7 +35,6 @@ public final class Constants {
 
     public static final double targetPositionError = 0.25;
     public static final double targetAngleError = 0.3;
-    
 
     public static enum SwerveLevel {
       L2(6.75, 21.428571428571427),
