fadc6fabfd5cd7d865c19e2e3f7ed9089b9cfd9e
arj2cool <arjunv.lzp@gmail.com>
2026-01-27 21:19:44 -0800

    auto changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,7 +16,6 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.commands.DriveToPose;
@@ -60,14 +59,14 @@ public class RobotContainer {
     autoChooser.addCmd(
         "sequence",
         () ->
-            new SequentialCommandGroup(
-                autoRoutines.getPathAsCommand(),
-                new InstantCommand(() -> DogLog.log("Choreo Command Ended", 1)),
-                new DriveToPose(
-                    drivetrain,
-                    () ->
-                        MiscUtils.plus(drivetrain.getCurrentState().Pose, new Translation2d(1, 0))),
-                new InstantCommand(() -> DogLog.log("Choreo Command Ended", 2))));
+            autoRoutines
+                .getPathAsCommand()
+                .andThen(
+                    new DriveToPose(
+                        drivetrain,
+                        () ->
+                            MiscUtils.plus(
+                                drivetrain.getCurrentState().Pose, new Translation2d(1, 0)))));
     SmartDashboard.putData("Auto Chooser", autoChooser);
 
     configureBindings();
