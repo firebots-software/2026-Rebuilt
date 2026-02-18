5a7a203ab060a4ee2e74c4847ea735558909aefa
Tim Shmidt <timsh007@gmail.com>
2026-02-18 15:57:45 -0800

    made new arclock cmd

--- src/main/java/frc/robot/commandGroups/ArcLock.java
+++ src/main/java/frc/robot/commandGroups/ArcLock.java
@@ -3,37 +3,38 @@ package frc.robot.commandGroups;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants;
-import frc.robot.commands.SwerveCommands.SwerveJoystickCommandWithPointing;
+import frc.robot.Constants.Landmarks;
+import frc.robot.commands.SwerveCommands.SwerveJoystickCommandInArc;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.HopperSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.util.Targeting;
+import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
-public class LockOnCommand extends ParallelCommandGroup {
-  public LockOnCommand(
+public class ArcLock extends ParallelCommandGroup {
+  public ArcLock(
       CommandSwerveDrivetrain drivetrain,
       ShooterSubsystem shooter,
-      IntakeSubsystem intake,
-      HopperSubsystem hopper,
-      DoubleSupplier frontBackFunction,
-      DoubleSupplier leftRightFunction,
+      DoubleSupplier tangentialVelocitySupplier,
       Pose3d target,
-      Joystick joystick) {
+      BooleanSupplier redside,
+      CommandXboxController joystick) {
 
     addCommands(
-        new SwerveJoystickCommandWithPointing(
-            frontBackFunction,
-            leftRightFunction,
-            () -> 1f,
-            () -> true,
-            () -> Targeting.targetAngle(target, drivetrain),
-            drivetrain),
+        new SwerveJoystickCommandInArc(
+            target,
+            tangentialVelocitySupplier,
+            (DoubleSupplier) (() -> 1f),
+            (BooleanSupplier) (() -> true),
+            (DoubleSupplier) (() -> Targeting.targetAngle(target, drivetrain)),
+            drivetrain,
+            redside),
         Commands.runEnd(
             () ->
                 joystick.setRumble(
@@ -45,5 +46,22 @@ public class LockOnCommand extends ParallelCommandGroup {
                 Units.metersToFeet(
                     Targeting.shootingSpeed(
                         target, drivetrain, Constants.Shooter.TARGETING_CALCULATION_PRECISION))));
+  }
+
+  public ArcLock(
+      CommandSwerveDrivetrain drivetrain,
+      ShooterSubsystem shooter,
+      IntakeSubsystem intake,
+      HopperSubsystem hopper,
+      DoubleSupplier tangentialVelocitySupplier,
+      BooleanSupplier redside,
+      CommandXboxController joystick) {
+    this(
+        drivetrain,
+        shooter,
+        tangentialVelocitySupplier,
+        redside.getAsBoolean() ? Landmarks.RED_HUB : Landmarks.BLUE_HUB,
+        redside,
+        joystick);
   }
 }
