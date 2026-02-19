f5dbf7ef446857532084a1a8f010f9e2d2a0790d
Tim Shmidt <timsh007@gmail.com>
2026-02-19 14:51:41 -0800

    added needed constant + build

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -530,6 +530,7 @@ public final class Constants {
       public static final double L3_REACH_POS = 0; // TODO: get vals
       public static final double PULL_DOWN_POS = 0; // TODO: get vals
       public static final double PULL_DOWN_POS_FOR_L1 = 0; // TODO: get vals
+      public static final double PULL_DOWN_POS_FOR_L2 = 0; // TODO: get vals
       public static final double PULL_DOWN_VELOCITY = -1; // TODO: get vals
 
       public static final int MOTOR_PORT_L = 9;
--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -24,7 +24,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.Constants.Climber.PullUp;
 import frc.robot.util.LoggedTalonFX;
 
 public class ClimberSubsystem extends SubsystemBase {
