aa90ccc66c1210d4c5f2b39bccb86b63ea1ba8c4
Sid Senthil <sidsenthil2010@gmail.com>
2026-03-01 12:31:07 -0800

    import constants

--- src/main/java/frc/robot/commandGroups/ShootBasicRetract.java
+++ src/main/java/frc/robot/commandGroups/ShootBasicRetract.java
@@ -2,6 +2,7 @@ package frc.robot.commandGroups;
 
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.Constants;
 import frc.robot.subsystems.HopperSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
@@ -19,9 +20,12 @@ public class ShootBasicRetract extends ParallelCommandGroup {
         (shooterSubsystem.shootAtSpeedCommand(speed)),
         Commands.waitUntil(() -> (shooterSubsystem.isAtSpeed() && readyToShoot.getAsBoolean()))
             .andThen(
-              hopperSubsystem.runHopperUntilInterruptedCommand()
-              .alongWith(intakeSubsystem.setArmToDegreesCommand(Constants.Intake.Arm.ARM_POS_RETRACTED)
-                         .beforeStarting(Commands.waitSeconds(0.5))));
+                hopperSubsystem
+                    .runHopperUntilInterruptedCommand()
+                    .alongWith(
+                        intakeSubsystem
+                            .setArmToDegreesCommand(Constants.Intake.Arm.ARM_POS_RETRACTED)
+                            .beforeStarting(Commands.waitSeconds(0.5)))));
 
     // addCommands(
     //     shooterSubsystem.shootAtSpeedCommand(speed),
