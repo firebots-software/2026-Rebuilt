fceff49da19d6694c551b9fc8b0ef826125d539d
Sid Senthil <sidsenthil2010@gmail.com>
2026-03-05 14:41:13 -0800

    create visionperiodic util

---
+++ src/main/java/frc/robot/util/VisionPeriodic.java
@@ -0,0 +1,10 @@
+package frc.robot.util;
+
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.VisionSubsystem;
+
+public final class VisionPeriodic {
+    public VisionPeriodic(VisionSubsystem visionFrontRight, VisionSubsystem visionFrontLeft, VisionSubsystem visionRearRight, VisionSubsystem visionRearLeft, CommandSwerveDrivetrain drivetrain) {
+        
+    }
+}
