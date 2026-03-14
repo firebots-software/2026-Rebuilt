b8ea168e1f20601e08633cdb27bbded6ac0145a0
Sid Senthil <sidsenthil2010@gmail.com>
2026-03-14 09:38:25 -0700

    add binds on buttonboard

--- src/main/java/frc/robot/util/NewCustomController.java
+++ src/main/java/frc/robot/util/NewCustomController.java
@@ -5,13 +5,21 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class NewCustomController extends GenericHID {
   Trigger skib;
+  Trigger intermapDown, intermapUp, intermapShoot;
 
   public NewCustomController(int port) {
     super(port);
     skib = new Trigger(() -> this.getRawButton(9));
+    intermapDown = new Trigger(() -> this.getRawButton(8));
+    intermapUp = new Trigger(() -> this.getRawButton(6));
+    intermapShoot = new Trigger(() -> this.getRawButton(7));
   }
 
   public Trigger Skib() {
     return skib;
   }
+
+  public Trigger IntermapDown() { return intermapDown; }
+  public Trigger IntermapUp() { return intermapUp; }
+  public Trigger IntermapShoot() { return intermapShoot; }
 }
