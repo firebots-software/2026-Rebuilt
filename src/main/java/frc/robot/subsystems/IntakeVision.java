35265d964d48a82ce994c7ec0a16e6bdf0a67b47
Sid Senthil <sidsenthil2010@gmail.com>
2026-03-19 15:10:33 -0700

    fix mising import and build

--- src/main/java/frc/robot/subsystems/IntakeVision.java
+++ src/main/java/frc/robot/subsystems/IntakeVision.java
@@ -1,14 +1,11 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 public class IntakeVision extends SubsystemBase {
 
-	public IntakeVision() {
-		
-	}
+  public IntakeVision() {}
 
-	@Override
-	public void periodic() {
-
-	}
-	
+  @Override
+  public void periodic() {}
 }
