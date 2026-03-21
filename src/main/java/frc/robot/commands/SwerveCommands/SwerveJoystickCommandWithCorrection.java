da34b430e25b5d8d09262289b3065f6190a35385
Tim Shmidt <timsh007@gmail.com>
2026-03-21 14:05:05 -0700

    rot corrections

---
+++ src/main/java/frc/robot/commands/SwerveCommands/SwerveJoystickCommandWithCorrection.java
@@ -0,0 +1,178 @@
+package frc.robot.commands.SwerveCommands;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.IntakeVisionDetection;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+public class SwerveJoystickCommandWithCorrection extends Command {
+  protected final DoubleSupplier xSpdFunction,
+      ySpdFunction,
+      turningSpdFunction,
+      speedControlFunction;
+  protected final BooleanSupplier fieldRelativeFunction,
+      doPointing,
+      redsideIfPointing,
+      doRotCorrection;
+
+  protected final CommandSwerveDrivetrain swerveDrivetrain;
+  protected final IntakeVisionDetection intakeVision;
+
+  private final SwerveRequest.FieldCentric fieldCentricDrive =
+      new SwerveRequest.FieldCentric().withDriveRequestType(DriveRequestType.Velocity);
+  private final SwerveRequest.RobotCentric robotCentricDrive =
+      new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.Velocity);
+  private boolean squaredTurn;
+
+  public SwerveJoystickCommandWithCorrection(
+      DoubleSupplier frontBackFunction,
+      DoubleSupplier leftRightFunction,
+      DoubleSupplier turningSpdFunction,
+      DoubleSupplier speedControlFunction,
+      BooleanSupplier fieldRelativeFunction,
+      BooleanSupplier doPointing,
+      BooleanSupplier doRotCorrection,
+      BooleanSupplier redSideIfPointing,
+      CommandSwerveDrivetrain swerveSubsystem,
+      IntakeVisionDetection intakeVision) {
+    this.xSpdFunction = frontBackFunction;
+    this.ySpdFunction = leftRightFunction;
+    this.turningSpdFunction = turningSpdFunction;
+    this.speedControlFunction = speedControlFunction;
+    this.fieldRelativeFunction = fieldRelativeFunction;
+    this.squaredTurn = true;
+    this.swerveDrivetrain = swerveSubsystem;
+    this.doPointing = doPointing;
+    this.doRotCorrection = doRotCorrection;
+    this.redsideIfPointing = redSideIfPointing;
+    this.intakeVision = intakeVision;
+
+    // Adds the subsystem as a requirement (prevents two commands from acting on subsystem at once)
+    addRequirements(swerveDrivetrain);
+  }
+
+  // Sets everything, not field relative
+  public SwerveJoystickCommandWithCorrection(
+      DoubleSupplier frontBackFunction,
+      DoubleSupplier leftRightFunction,
+      DoubleSupplier turningSpdFunction,
+      DoubleSupplier speedControlFunction,
+      CommandSwerveDrivetrain swerveSubsystem,
+      IntakeVisionDetection intakeVision) {
+
+    this(
+        frontBackFunction,
+        leftRightFunction,
+        turningSpdFunction,
+        speedControlFunction,
+        () -> false,
+        () -> false,
+        () -> false,
+        () -> false,
+        swerveSubsystem,
+        intakeVision);
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    // 1. Get real-time joystick inputs
+    double xSpeed = xSpdFunction.getAsDouble(); // xSpeed is actually front back (front +, back -)
+    double ySpeed = ySpdFunction.getAsDouble(); // ySpeed is actually left right (left +, right -)
+    double turningSpeed =
+        turningSpdFunction.getAsDouble(); // turning speed is (anti-clockwise +, clockwise -)
+
+    // 2. Normalize inputs
+    double length = xSpeed * xSpeed + ySpeed * ySpeed; // acutally length squared
+    if (length > 1d) {
+      length = Math.sqrt(length);
+      xSpeed /= length;
+      ySpeed /= length;
+    }
+
+    // Apply Square (will be [0,1] since `speed` is [0,1])
+    xSpeed = xSpeed * xSpeed * Math.signum(xSpeed);
+    ySpeed = ySpeed * ySpeed * Math.signum(ySpeed);
+    if (squaredTurn) {
+      turningSpeed = turningSpeed * turningSpeed * Math.signum(turningSpeed);
+    }
+    // 3. Apply deadband
+    xSpeed = Math.abs(xSpeed) > Constants.OI.LEFT_JOYSTICK_DEADBAND ? xSpeed : 0.0;
+    ySpeed = Math.abs(ySpeed) > Constants.OI.LEFT_JOYSTICK_DEADBAND ? ySpeed : 0.0;
+    turningSpeed =
+        Math.abs(turningSpeed) > Constants.OI.RIGHT_JOYSTICK_DEADBAND ? turningSpeed : 0.0;
+
+    // 4. Make the driving smoother
+    // This is a double between TELE_DRIVE_SLOW_MODE_SPEED_PERCENT and
+    // TELE_DRIVE_FAST_MODE_SPEED_PERCENT
+    double driveSpeed =
+        (Constants.Swerve.TELE_DRIVE_PERCENT_SPEED_RANGE * (speedControlFunction.getAsDouble()))
+            + Constants.Swerve.TELE_DRIVE_SLOW_MODE_SPEED_PERCENT;
+
+    // Applies slew rate limiter
+    xSpeed = xSpeed * driveSpeed * Constants.Swerve.PHYSICAL_MAX_SPEED_METERS_PER_SECOND;
+    ySpeed = ySpeed * driveSpeed * Constants.Swerve.PHYSICAL_MAX_SPEED_METERS_PER_SECOND;
+    turningSpeed =
+        turningSpeed * driveSpeed * Constants.Swerve.PHYSICAL_MAX_ANGLUAR_SPEED_RADIANS_PER_SECOND;
+
+    // Final values to apply to drivetrain
+    final double x = xSpeed;
+    final double y = ySpeed;
+
+    // final double turn =
+    //     (doPointing.getAsBoolean())
+    //         ? (swerveDrivetrain.calculateRequiredRotationalRate(
+    //             swerveDrivetrain.travelAngleTo(
+    //                 ((redsideIfPointing.getAsBoolean())
+    //                     ? (Constants.Landmarks.RED_HUB_2D)
+    //                     : (Constants.Landmarks.BLUE_HUB_2D)))))
+    //         : (turningSpeed);
+    double turn = 0d;
+    if (Math.abs(turningSpdFunction.getAsDouble()) > Constants.IntakeVision.OVERRIDE_ROT_INPUT) {
+      turn =
+          doRotCorrection.getAsBoolean()
+              ? swerveDrivetrain.calculateRequiredRotationalRate(
+                  new Rotation2d(
+                      Units.degreesToRadians(intakeVision.getYaw())
+                          + swerveDrivetrain.getPose().getRotation().getRadians()))
+              : turningSpeed;
+    }
+
+    turn =
+        (doPointing.getAsBoolean())
+            ? (swerveDrivetrain.calculateRequiredRotationalRateWithFF(
+                redsideIfPointing.getAsBoolean()
+                    ? Constants.Landmarks.RED_HUB_2D.getTranslation()
+                    : Constants.Landmarks.BLUE_HUB_2D.getTranslation()))
+            : (turningSpeed);
+
+    // 5. Applying the drive request on the swerve drivetrain
+    // Uses SwerveRequestFieldCentric (from java.frc.robot.util to apply module optimization)
+    SwerveRequest drive =
+        !fieldRelativeFunction.getAsBoolean()
+            ? fieldCentricDrive.withVelocityX(x).withVelocityY(y).withRotationalRate(turn)
+            : robotCentricDrive.withVelocityX(x).withVelocityY(y).withRotationalRate(turn);
+
+    // Applies request
+    this.swerveDrivetrain.setControl(drive);
+  } // Drive counterclockwise with negative X (left))
+
+  @Override
+  public void end(boolean interrupted) {
+    // Applies SwerveDriveBrake (brakes the robot by turning wheels)
+    this.swerveDrivetrain.setControl(new SwerveRequest.SwerveDriveBrake());
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
