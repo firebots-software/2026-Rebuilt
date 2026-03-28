043e8c67c7ceb2432688467ecc5cbdf006d809a4
arj2cool <arjunv.lzp@gmail.com>
2026-03-27 19:06:07 -0700

    left second dip

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -442,6 +442,7 @@ public final class Constants {
         SecondRightIntakeSweepShort,
         LeftSecondDip,
         RightSecondDip,
+        LeftSecondDipLong,
         RightSecondDipLong
       }
 
