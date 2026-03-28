00a9cd2a3f0b2e9b838d8aa4e8fc58f53877e162
Saketh Giri <217812618+sakky-sak@users.noreply.github.com>
2026-03-27 21:09:33 -0700

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -77,6 +77,7 @@ public class Robot extends TimedRobot {
     DogLog.log("Power/BatteryVoltage", RobotController.getBatteryVoltage());
     DogLog.log("Elastic/areWeActive", MiscUtils.areWeActive(simulatedTime));
     DogLog.log("Elastic/timeUntilNextShift", MiscUtils.countdownTillNextShift(simulatedTime));
+    SmartDashboard.putNumber("Elastic/timeUntilNextShift", MiscUtils.countdownTillNextShift(simulatedTime));
     DogLog.log("Elastic/currentShiftName", MiscUtils.currentShiftName(simulatedTime));
     MiscUtils.shiftSwitchIndicator(simulatedTime);
     simulatedTime -= 0.02;
