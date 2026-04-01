5a690919a9a47beebef30f29c2d3b92b4bb079c0
Sid Senthil <sidsenthil2010@gmail.com>
2026-03-31 20:53:04 -0700

    create motor configs

--- src/main/java/frc/robot/subsystems/HoodedShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/HoodedShooterSubsystem.java
@@ -3,7 +3,14 @@ package frc.robot.subsystems;
 import java.util.function.BooleanSupplier;
 
 import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.VoltageConfigs;
 import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -18,7 +25,7 @@ public class HoodedShooterSubsystem extends SubsystemBase {
     private double targetShooterSpeedRPS = 0;
     
 
-    public  HoodedShooterSubsystem(CommandSwerveDrivetrain drivetrain, BooleanSupplier redside) {
+    public HoodedShooterSubsystem(CommandSwerveDrivetrain drivetrain, BooleanSupplier redside) {
         this.drivetrain = drivetrain;
         this.redside = redside;
         warmup1 = new LoggedTalonFX("ShooterWarmup1", Constants.Shooter.WARMUP_1_ID, Constants.Swerve.CAN_BUS);
@@ -28,6 +35,16 @@ public class HoodedShooterSubsystem extends SubsystemBase {
         // TODO: fix hood id constant
         hood = new LoggedTalonFX("ShooterHood", Constants.Shooter.HOOD_ID, Constants.Swerve.CAN_BUS);
 
+        Slot0Configs s0c = new Slot0Configs().withKP(Constants.Shooter.KP).withKI(Constants.Shooter.KI).withKD(Constants.Shooter.KD).withKV(Constants.Shooter.KV).withKA(Constants.Shooter.KA);
+        CurrentLimitsConfigs clc = new CurrentLimitsConfigs().withStatorCurrentLimit(Constants.Shooter.STATOR_CURRENT_LIMIT).withSupplyCurrentLimit(Constants.Shooter.SUPPLY_CURRENT_LIMIT);
+        MotorOutputConfigs motorOutputConfigs = new MotorOutputConfigs().withInverted(InvertedValue.CounterClockwise_Positive).withNeutralMode(NeutralModeValue.Coast);
+        VoltageConfigs voltageConfigs = new VoltageConfigs().withPeakReverseVoltage(0.0);
+
+        TalonFXConfiguration config = new TalonFXConfiguration();
+        config.Slot0 = s0c;
+        config.CurrentLimits = clc;
+        config.MotorOutput = motorOutputConfigs;
+        config.Voltage =voltageConfigs;
     }
     
 }
