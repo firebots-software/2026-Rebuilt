3a827084db8294433add02c765e5c4a57facaa51
Tim Shmidt <timsh007@gmail.com>
2026-04-01 20:32:56 -0700

    now it checks for if you yearn to arc

--- src/main/java/frc/robot/commandGroups/ArcAroundAndShoot.java
+++ src/main/java/frc/robot/commandGroups/ArcAroundAndShoot.java
@@ -1,12 +1,14 @@
 package frc.robot.commandGroups;
 
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants;
 import frc.robot.Constants.Landmarks;
+import frc.robot.MathUtils.Vector3;
 import frc.robot.commands.SwerveCommands.SwerveJoystickCommandInArc;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.HopperSubsystem;
@@ -15,6 +17,7 @@ import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.util.Targeting;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
 
 public class ArcAroundAndShoot extends ParallelCommandGroup {
   public ArcAroundAndShoot(
@@ -32,10 +35,9 @@ public class ArcAroundAndShoot extends ParallelCommandGroup {
             target,
             tangentialVelocitySupplier,
             () -> 1f,
-            () -> false,
-            () -> Targeting.targetAngle(target, drivetrain),
-            drivetrain,
-            redside),
+            (BooleanSupplier) () -> false,
+            (Supplier<Translation2d>) () -> Vector3.toTranslation2d(Targeting.positionToTarget(target, drivetrain, Constants.Shooter.TARGETING_CALCULATION_PRECISION)),
+            drivetrain),
         Commands.runEnd(
             () ->
                 joystick.setRumble(
