bce4737daa7b032296db96268bcf01d028039381
arj2cool <arjunv.lzp@gmail.com>
2026-04-01 20:50:24 -0700

    not double

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -990,7 +990,7 @@ public final class Constants {
     public static final double ROBOT_FRONT_EDGE_TO_ROBOT_CENTER = 13.75d;
 
     public static final double HORIZONTAL_VELOCITY_OF_PROJECTILE = 5.0; //TUNE
-    public static final double PRECISION = 5.0; //TUNE
+    public static final double PRECISION = 5; //TUNE
 
 
     public static final InterpolatingDoubleTreeMap SHOOTER_WHEEL_RPS_FOR_DISTANCE_METERS =
