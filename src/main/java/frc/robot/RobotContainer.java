e39d06f8c85a00c162f45f8816fc2a887169c934
arj2cool <arjunv.lzp@gmail.com>
2026-04-05 18:06:03 -0700

    cmd

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commandGroups.ShootWithAimLeading;
 import frc.robot.commandGroups.ShootCommandGroups.ShootWithAim;
 import frc.robot.commands.SwerveCommands.SwerveJoystickDefaultCommand;
 import frc.robot.generated.TunerConstants;
@@ -158,6 +159,8 @@ public class RobotContainer {
                 redside,
                 secondController.visionShootingLockout()));
     secondController.reverseShoot().whileTrue(lebron.shootAtSpeedCommand(-45.0));
+
+    joystick.leftTrigger().whileTrue(new ShootWithAimLeading(frontBackFunction, leftRightFunction, lebron, intakeSubsystem, hopperSubsystem, drivetrain, redside, () -> false, () -> false));
   }
 
   public void visionPeriodic() {
