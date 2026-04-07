6c43bcf6212e54c83cd2d389f0b35dee519801a7
arj2cool <arjunv.lzp@gmail.com>
2026-04-06 21:13:42 -0700

    maybe

--- src/main/java/frc/robot/commandGroups/ShootCommandGroups/ShootPassing.java
+++ src/main/java/frc/robot/commandGroups/ShootCommandGroups/ShootPassing.java
@@ -1,16 +1,22 @@
 package frc.robot.commandGroups.ShootCommandGroups;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Constants;
 import frc.robot.commands.SwerveCommands.SwerveJoystickCommand;
+import frc.robot.commands.SwerveCommands.SwerveJoystickCommandWithPointing;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.HopperSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.util.Targeting;
+
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
 
 public class ShootPassing extends ParallelCommandGroup {
   public ShootPassing(
@@ -21,25 +27,22 @@ public class ShootPassing extends ParallelCommandGroup {
       HopperSubsystem hopper,
       CommandSwerveDrivetrain drivetrain,
       BooleanSupplier redside) {
-    Translation2d passingTranslation = drivetrain.getPassingTarget(redside);
+    Supplier<Translation2d> passingTranslation = () -> Targeting.computeVirtualTarget(new Pose2d(drivetrain.getPassingTarget(redside), new Rotation2d()), drivetrain);
     DoubleSupplier dist =
-        () -> drivetrain.getPose().getTranslation().getDistance(passingTranslation);
+        () -> drivetrain.getPose().getTranslation().getDistance(passingTranslation.get());
 
     addCommands(
         shooter.shootAtSpeedHoodCommand(
             () -> shooter.grabTargetShootingSpeed(dist.getAsDouble()),
             () -> shooter.grabTargetHoodAngle(dist.getAsDouble())),
-        new SwerveJoystickCommand(
+        new SwerveJoystickCommandWithPointing(
             translationalX,
             translationalY,
             () -> 0.0,
-            () -> 1.0,
             () -> false,
-            () -> false,
-            () -> true,
-            redside,
+            passingTranslation,
             drivetrain),
-        Commands.waitUntil(shooter::isShooterReady)
+        Commands.waitUntil(() -> (shooter.isShooterReady() && Targeting.pointingAtTarget(new Pose2d(drivetrain.getPassingTarget(redside), new Rotation2d()), drivetrain)))
             .andThen(
                 Commands.parallel(
                     hopper.runHopperUntilInterruptedCommand(
