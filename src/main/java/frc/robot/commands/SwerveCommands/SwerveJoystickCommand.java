e2afb27577d6e9e855a71a00dd3bcfd1f87a61a7
73anthonyL <73anthony.lu@gmail.com>
2026-04-24 18:02:47 -0700

    brake improvements but still broken

--- src/main/java/frc/robot/commands/SwerveCommands/SwerveJoystickCommand.java
+++ src/main/java/frc/robot/commands/SwerveCommands/SwerveJoystickCommand.java
@@ -2,9 +2,12 @@ package frc.robot.commands.SwerveCommands;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.util.Targeting;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
@@ -107,25 +110,17 @@ public class SwerveJoystickCommand extends Command {
     final double x = xSpeed;
     final double y = ySpeed;
 
-    // final double turn =
-    //     (doPointing.getAsBoolean())
-    //         ? (swerveDrivetrain.calculateRequiredRotationalRate(
-    //             swerveDrivetrain.travelAngleTo(
-    //                 ((redsideIfPointing.getAsBoolean())
-    //                     ? (Constants.Landmarks.RED_HUB_2D)
-    //                     : (Constants.Landmarks.BLUE_HUB_2D)))))
-    //         : (turningSpeed);
-
     double turn =
         (doPointing.getAsBoolean())
             ? (swerveDrivetrain.calculateRequiredRotationalRateWithFF(
                 swerveDrivetrain.getVirtualTarget(redsideIfPointing, () -> false)))
             : (turningSpeed);
 
-    if (doPassing.getAsBoolean())
+    if (doPassing.getAsBoolean()) {
       turn =
           swerveDrivetrain.calculateRequiredRotationalRateWithFF(
               swerveDrivetrain.getPassingTarget(redsideIfPointing));
+    }
 
     // 5. Applying the drive request on the swerve drivetrain
     // Uses SwerveRequestFieldCentric (from java.frc.robot.util to apply module optimization)
@@ -134,9 +129,24 @@ public class SwerveJoystickCommand extends Command {
             ? fieldCentricDrive.withVelocityX(x).withVelocityY(y).withRotationalRate(turn)
             : robotCentricDrive.withVelocityX(x).withVelocityY(y).withRotationalRate(turn);
 
-    // Applies request
-
-    if (braking.getAsBoolean()) {
+    boolean isPointed =
+        Targeting.pointingAtTarget(
+            swerveDrivetrain
+                    .travelAngleTo(
+                        new Pose2d(
+                            swerveDrivetrain.getVirtualTarget(redsideIfPointing, () -> false),
+                            new Rotation2d()))
+                    .getRadians()
+                + Math.PI,
+            swerveDrivetrain);
+    boolean intentionallyStationary =
+        (xSpdFunction.getAsDouble() < 0.1)
+            && (ySpdFunction.getAsDouble() < 0.1)
+            && (turningSpdFunction.getAsDouble() < 0.1);
+    if (braking.getAsBoolean()
+        && doPointing.getAsBoolean()
+        && isPointed
+        && intentionallyStationary) {
       swerveDrivetrain.brakeSwerve();
     } else {
       this.swerveDrivetrain.setControl(drive);
